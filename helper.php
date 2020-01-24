4851ddf056f8659336cdb948b1ce0b451c5957a6
Tim <git@nztm.net>
2020-01-25 07:43:31 +1300

    Add error handling and caching

--- helper.php
+++ helper.php
@@ -4,17 +4,32 @@ use NZTim\SimpleHttp\Http;
 
 function geolocate(string $ip, int $timeout = 5): string
 {
+    // Skip unit tests
+    if (app()->runningUnitTests()) {
+        return 'NZ';
+    }
+    // Validate $ip
     $ip = trim($ip);
     if (!filter_var($ip, FILTER_VALIDATE_IP)) {
         return '??';
     }
+    // Get result
     $domain = config('services.geo_domain', 'example.org');
     $url = "https://{$domain}/locate?ip={$ip}";
-    $response = (new Http())
-        ->timeout($timeout)
-        ->get($url);
+    $key = 'geo-' . md5($ip);
+    if (cache()->has($key)) {
+        return cache($key);
+    }
+    try {
+        $response = (new Http())->timeout($timeout)->get($url);
+    } catch (Throwable $e) {
+        log_warning('comms', 'Geolocate helper failed: ' . $e->getMessage());
+        return '??';
+    }
     if (!$response->isOk()) {
         return '??';
     }
-    return str_limit($response->body(), 2);
+    $result = str_limit(trim($response->body()), 2);
+    cache()->put($key, $result, now()->addWeek());
+    return $result;
 }
